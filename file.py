b53b2e029c3a23caba23c1053a6c0af144d146e1
itzplayz0001 <66119667+itzplayz0001@users.noreply.github.com>
2020-08-27 21:07:59 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,54 @@
+# name = input("Hello! What is your name?")
+# print("Okay! Hello, " + name)
+# color = input("Now what's your fav color?")
+# print("so this is what i know about you, your name is " + name + "and you like " + color)
+
+#----- CONVERTING POUNDS TO KILO GRAM
+# pounds = input("Please enter your weight in pounds: ")
+# kg = int(pounds)*0.45
+# print(kg)
+
+#----- Getting the prefix of text
+#text = '!hello'
+ #       012345  <--- PREFIX
+ #      -5   -1  <--- SUFIX
+ # Get first number of digits by do ing 0:{number of digits you want from number 0 here 0 will be considered as 1}
+ # EXCLUDE PREFIX AND SUFIX BY [1:-1]
+#print(text[0])
+
+ #----- Formatted Strings
+#first = 'Bhavit'
+#last = 'Rao'
+#msg = f'{first} [{last}] is a coder'
+#print(msg)
+
+ #----- get the length of the word(s) or sentence
+#text = 'Hello Everyone'
+#print(len(text))
+
+#----- Make all to upper case
+#course = 'this is a test'
+#print(course.upper())
+# make it lower case for lower case text
+
+#----- REPLACE TEXT
+#text = 'Sunitha is a dentist'
+#print(text.replace('dentist', 'dental doc'))
+
+#-----KNOW IF A WORD EXISTS IN A SENTENCE
+#text = 'C SHARP'
+#print('python' in text)
+# this finds the word python in the var text and says true or false
+
+#----- VOTING SYSTEM
+#name = input("Please enter your name: ")
+#print("Hello, " + name)
+#age = int(input("Please enter your age: "))
+#if (age < 18):
+# print(name + ", You are not eligible to vote")
+#elif(age > 18):
+# print(name + ", You are eligible to vote")
+
+#-----
+
+
---
+++ file1.py
@@ -0,0 +1,26 @@
+#name = input("Please enter your username: ")
+#
+#if len(name) < 3:
+#    print("Your name cannot be lesser than 3 charecters...")
+#
+#elif len(name) > 14:
+#      print("Your name is too big")
+#else:
+#    print("Name looks good")
+#----------------------------------------------     
+#w = int(input("Please enter your weight"))
+#u = input('Lbs  or Kgs')
+#
+#if u.upper() == 'L':
+#    c = w * 0.45
+#    print(f"You are {c} kilos")
+#else:
+#    cc = w / 0.45
+#    print(f"You are {cc} Pounds!")
+#----------------------------------------------
+# NUMBER SPAM 
+#i = 1
+#while 1 <= 5:
+#    i = i + 50000000007584765836457346587
+#    print(i)
+
