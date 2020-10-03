79b6c8264916ee8fb3a4c8f251fddc85ea7b4791
itzplayz0001 <66119667+itzplayz0001@users.noreply.github.com>
2020-10-03 10:09:44 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,6 @@
+object = ["ItzPLAYZ", "15", "C#", "https://github.com/itzplayz0001"]
+print("Hey there my name is", object[0], ", I love coding in", object[2], ". You can check my github here", object[3])
+
+# to delete an object 
+
+del object[1] #this will delete 15 from the list
