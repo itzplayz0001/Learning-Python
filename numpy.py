a0d14901271652d8d7dcda8764faaf927814897c
itzplayz0001 <66119667+itzplayz0001@users.noreply.github.com>
2021-03-08 21:30:59 +0530

    Create numpy.py

---
+++ numpy.py
@@ -0,0 +1,12 @@
+import numpy 
+
+# Making a list to a numpy array...
+arandomlist = [1, 2, 4, 8, 3, 6]
+
+myarray = numpy.array(arandomlist)
+print(myarray) 
+ # OUTPUT - [1 2 4 8 3 6]
+  
+  
+# Ascending order 
+# We use the numpy array for this...
